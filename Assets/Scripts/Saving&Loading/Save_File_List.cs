8d63a3f8fe997d58ec52f6941b68d93880ee7df4
batomow <carlosa_miranda@outlook.com>
2017-05-25 17:04:06 -0700

    defined savind standards and started by runnig some test on the playhourse per state

---
+++ Assets/Scripts/Saving&Loading/Save_File_List.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+using SavingStandars; 
+
+public class Save_File_List : MonoBehaviour {
+
+	public Text[] saves; 
+
+	private float auxTime; 
+	private int auxHours, auxMinutes, auxSeconds; 
+
+	void Awake(){
+		for (int n = 0; n < saves.Length; n++) {
+			if (PlayerPrefs.HasKey (Keys.dataKey (n))) {
+				auxTime = PlayerPrefs.GetFloat (Keys.dataKey (n)); 
+				auxHours = Mathf.FloorToInt (auxTime / 3600); 
+				auxMinutes =  Mathf.Abs(Mathf.FloorToInt ((auxHours * 60) - Mathf.FloorToInt (auxTime/60)));
+				auxSeconds = Mathf.FloorToInt (auxTime % 60);
+				saves [n].text = "Save " + (n + 1) + ": " + auxHours + "h:" + auxMinutes + "m:" + auxSeconds + "s"; 
+			} else {
+				saves [n].text = "Save " + (n + 1) + ": Blank"; 
+			}
+		}
+	}
+
+	void Start(){
+
+	}
+
+
+
+	/*double getSavedData(){
+		
+	}*/
+
+}
---
+++ Assets/Scripts/Saving&Loading/SavingStandars.cs
@@ -0,0 +1,37 @@
+﻿namespace SavingStandars  {
+
+	/// <summary>
+	/// Contains all they general keys for saving and loading; 
+	/// </summary>
+	public static class Keys{
+		public static string dataKey(int savefilenumber){
+			return "savedata" + savefilenumber.ToString(); 
+		}
+		public static string playerPositionx(int savefilenumber, int subindex){
+			return "savedata" + savefilenumber.ToString() + "playerposx" + subindex.ToString(); 
+		}
+		public static string playerPositiony(int savefilenumber, int subindex){
+			return "savedata" + savefilenumber.ToString() + "playerposy" + subindex.ToString(); 
+		}
+		public static string monsterAvailable(int savefilenumber, int subindex){
+			return "savedata" + savefilenumber.ToString() + "monsterav" + subindex.ToString(); 
+		}
+		public static string monsterExperience(int savefilenumber, int subindex){
+			return "savedata" + savefilenumber.ToString() + "monsterexp" + subindex.ToString(); 
+		}
+		public static string monsterHealth(int savefilenumber, int subindex){
+			return "savedata" + savefilenumber.ToString() + "monsterhealth" + subindex.ToString(); 
+		}
+		public static string npcstate(int savefilenumber, int subindex){
+			return "savedata" + savefilenumber.ToString() + "statenpc" + subindex.ToString(); 
+		}
+		public static string npcpositionx(int savefilenumber, int subindex){
+			return "savedata" + savefilenumber.ToString() + "npcposx" + subindex.ToString(); 
+		}
+		public static string npcpositiony(int savefilenumber, int subindex){
+			return "savedata" + savefilenumber.ToString() + "npcposy" + subindex.ToString(); 
+		}
+	}
+
+
+}
