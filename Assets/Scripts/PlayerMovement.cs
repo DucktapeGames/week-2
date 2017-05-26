b3b7c84d2e7f312270453e3c63f8619632b08bd1
julio177 <julioc.aguilarv@gmail.com>
2017-05-26 17:45:35 -0500

    Corrige movimiento del player wuwwu

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,30 +9,47 @@ public class PlayerMovement : MonoBehaviour {
     Vector2 mapPosition;
  	public float speed;   // Speed of movement
     public GridMap map;
+    bool flag = false;
      
     void Start () {
         // Take the initial position
         mapPosition = map.ToMapPosition(transform.position);
         worldPosition = map.ToWorldPosition(mapPosition);
+        StartCoroutine(DetectInput());
     }
 
     void FixedUpdate () {
-        if(Input.GetKey(KeyCode.A) && map.ToWorldPosition(mapPosition) == worldPosition) {        // Left
-            mapPosition += Vector2.left;
-            worldPosition = map.ToWorldPosition(mapPosition);
+         transform.position = Vector3.MoveTowards(transform.position, worldPosition, Time.deltaTime * speed);
+    }
+
+    IEnumerator DetectInput() {
+        while(true) {
+            if(Input.GetKey(KeyCode.A) && map.ToWorldPosition(mapPosition) == worldPosition && !flag) {        // Left
+                mapPosition += Vector2.left;
+                worldPosition = map.ToWorldPosition(mapPosition);
+                flag = true;
+            }
+            else if(Input.GetKey(KeyCode.D) &&  map.ToWorldPosition(mapPosition) == worldPosition && !flag) {        // Right
+                mapPosition += Vector2.right;
+                worldPosition = map.ToWorldPosition(mapPosition);
+                flag = true;
+            }
+            else if(Input.GetKey(KeyCode.S) &&  map.ToWorldPosition(mapPosition) == worldPosition && !flag) {        // Up
+                mapPosition += Vector2.up;
+                worldPosition = map.ToWorldPosition(mapPosition);
+                flag = true;
+            }
+            else if(Input.GetKey(KeyCode.W) &&  map.ToWorldPosition(mapPosition) == worldPosition && !flag) {        // Down
+                mapPosition += Vector2.down;
+                worldPosition = map.ToWorldPosition(mapPosition);
+                flag = true;
+            }
+
+            if((transform.position - worldPosition).magnitude < 0.02) {
+                flag = false;
+            }
+        
+            yield return new WaitForSeconds(0.1f);
         }
-        if(Input.GetKey(KeyCode.D) &&  map.ToWorldPosition(mapPosition) == worldPosition) {        // Right
-            mapPosition += Vector2.right;
-            worldPosition = map.ToWorldPosition(mapPosition);
-        }
-        if(Input.GetKey(KeyCode.S) &&  map.ToWorldPosition(mapPosition) == worldPosition) {        // Up
-            mapPosition += Vector2.up;
-            worldPosition = map.ToWorldPosition(mapPosition);
-        }
-        if(Input.GetKey(KeyCode.W) &&  map.ToWorldPosition(mapPosition) == worldPosition) {        // Down
-            mapPosition += Vector2.down;
-            worldPosition = map.ToWorldPosition(mapPosition);
-        }
-        transform.position = Vector3.MoveTowards(transform.position, worldPosition, Time.deltaTime * speed);    // Move there
     }
 }
